dfa339fa45c8c1c5b04f76086bd44249267eec5b
AxelRSa <axelg.rizos@gmail.com>
2021-12-13 14:46:16 -0500

    init

---
+++ src/modules/Home/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ProductCard description="hola" />
+  </div>
+</template>
+<script>
+import ProductCard from '@/modules/Home/Hero/ProductCard.vue'
+export default {
+  components: {
+    ProductCard
+  }
+}
+</script>
---
+++ src/modules/Home/Hero/ProductCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hola">
+    <PText>{{ description }}</PText>
+  </div>
+</template>
+<script>
+import PText from '@/components/PText.vue'
+export default {
+  components: {
+    PText
+  },
+  props: ['description']
+}
+</script>
+<style scoped>
+div {
+  background-color: blue;
+}
+</style>
